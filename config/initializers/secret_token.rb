a594334a798a0068f74e531bd530eff5636c7ea9
Freeman Fan <fanfreeman@gmail.com>
2013-05-03 22:50:06 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainbowJump::Application.config.secret_token = 'c66a2231d991e61a38f9138987e62886238cc502a76ea29a31f273de2d9e2de7d0521807d2d8f35df609f79268e9b2ff278b59f5beaa4535bac3137d810fe7ce'
